5b53b079d53371ff2f340e0432a2b7e33c6c6325
Ethan Haugh <ethanhaugh201@gmail.com>
2026-01-14 12:45:33 +1100

    Fix some Trading212 API Changes

--- python/src/consts/consts.py
+++ python/src/consts/consts.py
@@ -1,8 +1,11 @@
 import os
 
-API_KEY = os.getenv("TRADING212KEY")
 
-GENERATE_REPORT_URL = "https://live.trading212.com/api/v0/history/exports"
+API_KEY = os.getenv("TRADING212KEYID")
+
+API_KEY_SECRET = os.getenv("TRADING212KEY")
+
+GENERATE_REPORT_URL = "https://live.trading212.com/api/v0/equity/history/exports"
 
 RETRIEVE_REPORT_URL = "https://live.trading212.com/api/v0/history/exports"
 
@@ -10,4 +13,4 @@ RETRIEVE_OPEN_POSITIONS_URL = "https://live.trading212.com/api/v0/equity/portfol
 
 RETRIEVE_ACCOUNT_CASH_URL = "https://live.trading212.com/api/v0/equity/account/cash"
 
-REQUEST_HEADERS = {"Authorization": API_KEY}
+REQUEST_HEADERS = {"Authorization": API_KEY_SECRET}
